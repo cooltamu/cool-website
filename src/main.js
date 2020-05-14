1e2eeddaa5b5c12cd5d0804770a9dc47d8bd7eb8
Jonathan Davis Samuel <jone.bala@gmail.com>
2020-05-14 00:47:22 -0500

    initial push

--- src/main.js
+++ src/main.js
@@ -1,23 +1,33 @@
 import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker.js'
-import router from './router'
+import '@/plugins/axios'
+import vuetify from '@/plugins/vuetify'
+import '@/plugins/veevalidate'
+import '@/plugins/common'
+import '@/plugins/googleAnalytics'
+import i18n from '@/plugins/i18n'
+import App from '@/App.vue'
+import router from '@/router'
+import { store } from '@/store'
+import VuetifyConfirm from 'vuetify-confirm'
 
 Vue.config.productionTip = false
+Vue.use(VuetifyConfirm, { vuetify })
 
-
-
-
-new Vue({
+const app = new Vue({
+  vuetify,
   router,
-  render: h => h(App),
-  data:{  snapshotItems : [
-    {
-      ref: "#",
-      imageRef:"about.jpg",
-      title:"test",
+  store,
+  i18n,
+  render: (h) => h(App),
+  created() {
+    store.dispatch('setLocale', store.getters.locale)
+    if (store.getters.isTokenSet) {
+      store.dispatch('autoLogin')
     }
-],
-}
-
+  }
 }).$mount('#app')
+
+if (window.Cypress) {
+  // Only available during E2E tests
+  window.app = app
+}
