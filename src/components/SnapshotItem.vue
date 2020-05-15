b908a3c7a749a1a33af24ba0f555c0dca3ceb39c
Jonathan Davis Samuel <jone.bala@gmail.com>
2020-05-15 02:11:03 -0500

    Add static site
    To swtich to static site, on main.js un/comment lines 7 and 8

--- src/components/SnapshotItem.vue
+++ src/components/SnapshotItem.vue
@@ -3,8 +3,13 @@
     <div class="container">
       <div class="row justify-content-center pb-3">
         <div class="col-md-10 heading-section text-center ftco-animate">
-          <h2 class="mb-4">Let's build the world without child abuse and neglect</h2>
-          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+          <h2 class="mb-4">
+            Let's build the world without child abuse and neglect
+          </h2>
+          <p>
+            Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia
+          </p>
         </div>
       </div>
     </div>
@@ -14,26 +19,38 @@
           <div class="carousel-causes owl-carousel">
             <div class="item">
               <a href="causes.html" class="causes text-center">
-                <div class="img" style="background-image: url(images/causes-1.jpg);"></div>
+                <div
+                  class="img"
+                  style="background-image: url(images/causes-1.jpg);"
+                ></div>
                 <h2>Adoption, Fostering &amp; Children Care</h2>
               </a>
             </div>
 
             <div class="item">
               <a href="causes.html" class="causes text-center">
-                <div class="img" style="background-image: url(images/causes-3.jpg);"></div>
+                <div
+                  class="img"
+                  style="background-image: url(images/causes-3.jpg);"
+                ></div>
                 <h2>Meditation &amp; Crisis Services</h2>
               </a>
             </div>
             <div class="item">
               <a href="causes.html" class="causes text-center">
-                <div class="img" style="background-image: url(images/causes-4.jpg);"></div>
+                <div
+                  class="img"
+                  style="background-image: url(images/causes-4.jpg);"
+                ></div>
                 <h2>Providing Children Care and Education</h2>
               </a>
             </div>
             <div class="item">
               <a href="causes.html" class="causes text-center">
-                <div class="img" style="background-image: url(images/causes-5.jpg);"></div>
+                <div
+                  class="img"
+                  style="background-image: url(images/causes-5.jpg);"
+                ></div>
                 <h2>Safeguarding &amp; Consultancy Services</h2>
               </a>
             </div>
@@ -42,7 +59,6 @@
       </div>
     </div>
   </section>
-
 </template>
 
 <script>
@@ -55,6 +71,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,26 @@
 <template>
   <div class="home">
-
-    <Volunteers/>
-    <Mission/>
-<Snapshot/>
+    <Volunteers />
+    <Mission />
+    <Snapshot />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import NavBar from '@/components/NavBar.vue'
- import Volunteers from '@/components/Volunteers.vue'
- import Mission from '@/components/Mission.vue'
-  import Snapshot from '@/components/Snapshot.vue'
+import Volunteers from '@/components/Volunteers.vue'
+import Mission from '@/components/Mission.vue'
+import Snapshot from '@/components/Snapshot.vue'
 
 export default {
   name: 'Home',
   components: {
     // NavBar,
-     Volunteers,
-     Mission,
-     Snapshot
+    Volunteers,
+    Mission,
+    Snapshot
   }
 }
 </script>
-<style>
-
-</style>
+<style></style>
