7831cad2558f3229351983023bc8a39d8145b47b
Jonathan Davis Samuel <jone.bala@gmail.com>
2020-05-16 00:35:23 -0500

    First version of the COOL site

--- src/components/core/Footer.vue
+++ src/components/core/Footer.vue
@@ -1,11 +1,8 @@
 <template>
-  <v-footer height="60" class="d-block text-center">
+  <v-footer height="40" class="d-block text-center">
     <div>
-      v{{ appVersion }} - {{ $t('footer.MADE_WITH') }}
-      <v-icon small class="red--text mx-0 px-0">mdi-cards-heart</v-icon>
-      &nbsp;{{ $t('footer.BY') }}:
+      Copyright &copy; 2020 COOL | All rights reserved
     </div>
-    <a href="https://daniel-avellaneda.com">https://daniel-avellaneda.com</a>
   </v-footer>
 </template>
 
