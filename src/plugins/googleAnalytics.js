eba500b7b767f0825d9c26a935141c82888a554b
iconjone <jone.bala@gmail.com>
2020-05-23 06:09:10 -0500

    Revert "Revert "Fix: google anal, temp feat: add events button""
    This reverts commit afee7cce5d3ae46377b5c4397fa5792d7fd595ff.

--- src/plugins/googleAnalytics.js
+++ src/plugins/googleAnalytics.js
@@ -4,7 +4,7 @@ import VueAnalytics from 'vue-analytics'
 
 if (process.env.NODE_ENV === 'production') {
   Vue.use(VueAnalytics, {
-    id: 'UA-138050457-1',
+    id: 'UA-167109323-1',
     router
   })
 }
